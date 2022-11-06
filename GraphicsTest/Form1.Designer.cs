c4912a5e0c3b926832e0600959541c5325660afa
GalaxyVN <phamgiahuy092@gmail.com>
2022-11-06 20:07:48 +0700

    init

---
+++ GraphicsTest/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace LaiSuatCalculation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btplay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(801, 395);
+            this.panel1.TabIndex = 0;
+            // 
+            // btplay
+            // 
+            this.btplay.Location = new System.Drawing.Point(0, 1);
+            this.btplay.Name = "btplay";
+            this.btplay.Size = new System.Drawing.Size(126, 56);
+            this.btplay.TabIndex = 1;
+            this.btplay.Text = "Play";
+            this.btplay.UseVisualStyleBackColor = true;
+            this.btplay.Click += new System.EventHandler(this.btplay_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 261);
+            this.Controls.Add(this.btplay);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button btplay;
+
+        private System.Windows.Forms.Panel panel1;
+
+        #endregion
+    }
+}
---
+++ GraphicsTest/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LaiSuatCalculation
+{
+    public partial class Form1 : Form
+    {
+
+        public volatile Graphics ggg;
+        
+        public Form1() 
+        {
+            InitializeComponent();
+            ggg = panel1.CreateGraphics();
+        }
+
+        private void btplay_Click(object sender, EventArgs e)
+        {
+            int i = 10;
+            Rectangle rect2 = new Rectangle(10, 10, 20, 20);
+            ggg.DrawRectangle(Pens.Aqua, rect2);
+
+            while (i < 150)
+            {
+                i += 1;
+                Thread.Sleep(100);
+                ggg.Clear(Color.White);
+                Rectangle rect1 = new Rectangle(10, i, 20, 20);
+                ggg.DrawRectangle(Pens.Aqua, rect1);
+            }
+        }
+    }
+}
